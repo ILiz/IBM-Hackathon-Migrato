1d72d7625ba4a10b3c71bffd2d7a1f6c1553279f
JurrienT <jurrien.theunisz@migrato.nl>
2024-09-22 16:22:01 +0200

    Adds basic design and styling.

--- Frontend/my-react-app/src/App.js
+++ Frontend/my-react-app/src/App.js
@@ -1,77 +1,140 @@
 import './App.css';
 import { useRef } from 'react';
-import {
-    TextField
-} from "@mui/material";
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import { useState } from "react";
+import { createElement } from 'react';
+import TextField from "@mui/material/TextField";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import Logo from "./images/logo.png";
+import User from "./images/user.png";
 
 function App() {
-    // Create a reference to the hidden file input element
   const hiddenFileInput = useRef(null);
-
-  const handleClick = event => {
+  const [askDisabled, setAskDisabled] = useState(true);
+  const [list, setList] = useState();
+  
+    //Called when a selected file is uploaded.
+  const handleUploadFile = event => {
     hiddenFileInput.current.click();
   };
-
-  // Call a function (passed as a prop from the parent component)
-  // to handle the user-selected file
-  const handleChange = event => {
-    const fileUploaded = event.target.files[0];
-    // TODO we have the file, now process it
+  
+  //Called when the selected file changes.
+  const onFileChanged = event => {
+	console.log("Uploading document");
+    uploadFile(event.target.files[0]);
   };
+  
+  //Uploads a file.
+  const uploadFile = async file => {
+	//console.log(fileUploaded['name'])
+	//const formdata = new FormData();
+	//formdata.append("file_upload", fileUploaded);
+	//const response = await fetch('http://127.0.0.1:8000/api/v1/documentSubmitIndexing', {
+	//	method: 'POST',
+	//	body: formdata // Here, stringContent or bufferContent would also work
+	//})
+	//const data = await response.json();
+	//if (data === "File uploaded and indexed") {
+	//	setFilename(fileUploaded['name'])
+	//}  
+	
+	console.log(file);
+	
+	//let newFile = createElement(
+	//  "h1",
+	//  { className: 'greeting' },
+	//  { style: { color: "blue" } },
+	//  "This is a welcome text"
+	//);
+	//const newList = list.concat({newFile});
+	//setList(newList);
+  };
+  
+  //Called when the user question changes.
+  const onQuestionChanged = event => {	
+	setAskDisabled(!event.target.value);
+  };
+  
+  //Called when the user question is submitted.
+  const handleSubmitQuestion = event => {
+	console.log(event.target.value);
+	event.target.value = "";
+	setAskDisabled(true);
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <button className="button-upload" onClick={handleClick}>
-            Upload a file
-        </button>
-        <input
-            type="file"
-            onChange={handleChange}
-            ref={hiddenFileInput}
-            style={{display: 'none'}} // Make the file input element invisible
-        />
-      </header>
+	<div className="App">
+		<header className="Top-bar">
+			<div className="Top-bar-left">
+			</div>
+			<div className="Top-bar-center">
+				<img src={Logo} alt="Logo" className="Image-logo"/>
+			</div>
+			<div className="Top-bar-right">
+				<img src={User} alt="User" className="Image-user"/>
+			</div>
+		</header>
+		
+		<div className="Left-sidebar">
+			<div className="Document-header">
+				<h1 id="documentHeader">Documents</h1>
+			</div>
+			<div className="Document-list">
+				<List id="documentList" list={list}></List>
+			</div>
+			<div className="Document-buttons">
+				<Button variant="contained" id="uploadButton" onClick={handleUploadFile} startIcon={<CloudUploadIcon />}>
+					Upload document
+				</Button>
+				<input
+					type="file"
+					onChange={onFileChanged}
+					ref={hiddenFileInput}
+					style={{display: 'none'}} // Make the file input element invisible
+				/>
+			</div>
+		</div>
 
-      <div className="bodyChat">
-        <div className="inputField" >
-            <TextField id="userInput" variant="standard" fullWidth
-                InputLabelProps={{
-                    style: {
-                        textOverflow: 'ellipsis',
-                        whiteSpace: 'nowrap',
-                        overflow: 'hidden',
-                        width: '100%',
-                        color: 'white'
-                    }
-                }}
-                sx={{
-                    input: {
-                        color: "white",
-                        borderBottom: "2px solid gray",
-                    },
-                    "& .MuiInput-underline:after": {
-                        borderBottomColor: "orange"
-                    }
-                }}
-                onKeyDown={(ev) => {
-                     if (ev.key === 'Enter') {
-                         //postUserInput()
-                         ev.target.value = ""
-                     }
-                }}
-                onChange={(ev) => {
-                     //setInputTextVal(ev.target.value)
-                }}
-                //value={inputTextVal}
-            />
-        </div>
-            <div className="questionBoxRightIcon">
-                 <ArrowUpwardIcon className="arrowUp" style={{color: 'black'}} />
-             </div>
-       </div>
-    </div>
+		<div className="App-center">
+			<div className="Chatbox">
+				<div className="Feedback-box">
+				</div>
+				<div className="Input-box" >
+					<TextField id="userInput" placeholder="Ask something..." variant="standard"
+						InputProps={{ 
+							disableUnderline: true 
+						}}
+						sx={{
+							input: {
+								color: "black",
+							}
+						}}
+						onKeyDown={(ev) => {
+							 if (ev.key === 'Enter') {
+								 handleSubmitQuestion(ev)
+							 }
+						}}
+						onChange={(ev) => {
+							 onQuestionChanged(ev)
+						}}
+					/>
+					<div className="Input-buttons">
+						<Button variant="contained" disabled={askDisabled} endIcon={<SendIcon />}>
+						  Ask
+						</Button>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</div>
   );
 }
 
