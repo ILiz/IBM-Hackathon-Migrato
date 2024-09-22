d159eed5ecf5e2bf3d15da3a8728139e51bc3ab1
JurrienT <jurrien.theunisz@migrato.nl>
2024-09-22 18:04:50 +0200

    Adds file list UI.

--- Frontend/my-react-app/src/App.js
+++ Frontend/my-react-app/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { useRef } from 'react';
 import { useState } from "react";
-import { createElement } from 'react';
 import TextField from "@mui/material/TextField";
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -18,9 +17,9 @@ import User from "./images/user.png";
 function App() {
   const hiddenFileInput = useRef(null);
   const [askDisabled, setAskDisabled] = useState(true);
-  const [list, setList] = useState();
+  const [files, setFiles] = useState([]);
   
-    //Called when a selected file is uploaded.
+  //Called when a selected file is uploaded.
   const handleUploadFile = event => {
     hiddenFileInput.current.click();
   };
@@ -33,64 +32,61 @@ function App() {
   
   //Uploads a file.
   const uploadFile = async file => {
-    const formdata = new FormData();
-    formdata.append("file_upload", file);
+    //const formdata = new FormData();
+    //formdata.append("file_upload", file);
 
-    fetch('http://127.0.0.1:8000/api/v1/documentSubmitIndexing', {
-      method: 'POST',
-      body: formdata // Here, stringContent or bufferContent would also work
-    })
-    .then(function(res) {
-      return res.json();
-    }).then(function(json) {
-      console.log(json);
-    });
-	//console.log(fileUploaded['name'])
-	//const formdata = new FormData();
-	//formdata.append("file_upload", fileUploaded);
-	//const response = await fetch('http://127.0.0.1:8000/api/v1/documentSubmitIndexing', {
-	//	method: 'POST',
-	//	body: formdata // Here, stringContent or bufferContent would also work
-	//})
-	//const data = await response.json();
-	//if (data === "File uploaded and indexed") {
-	//	setFilename(fileUploaded['name'])
-    //}
+    //fetch('http://127.0.0.1:8000/api/v1/documentSubmitIndexing', {
+    //  method: 'POST',
+    //  body: formdata // Here, stringContent or bufferContent would also work
+    //})
+    //.then(function(res) {
+    //  return res.json();
+    //}).then(function(json) {
+    //  console.log(json);
+    //});
 	
-	console.log(file);
-	
-	//let newFile = createElement(
-	//  "h1",
-	//  { className: 'greeting' },
-	//  { style: { color: "blue" } },
-	//  "This is a welcome text"
-	//);
-	//const newList = list.concat({newFile});
-	//setList(newList);
+	onFileUploadSuccess(file);
+  };
+  
+  //Called when a file has been uploaded successfully.
+  const onFileUploadSuccess = file => {
+	const filename = file.name;
+	console.log("Uploaded: " + filename);	
+	setFiles(
+	  [
+		...files,
+		{ uuid: filename, name:filename, url:"?" }//TODO: Create object from response with delete link etc.
+	  ]
+	);
   };
   
   //Called when the user question changes.
-  const onQuestionChanged = event => {	
-	setAskDisabled(!event.target.value);
+  const onQuestionChanged = question => {	
+	setAskDisabled(!question);
   };
   
-  //Called when the user question is submitted.
-  const handleSubmitQuestion = event => {
-	console.log(event.target.value);
-	event.target.value = "";
+  //Called when the submit question button is clicked.
+  const handleAskButtonClick = () => {
+	handleSubmitQuestion(document.getElementById('userInput'));
+  }
+  
+  //Called when the user question is submitted.  
+  const handleSubmitQuestion = textfield => {
+	const question = textfield.value; 
+	textfield.value = "";
 	setAskDisabled(true);
-	console.log("sending question") ;
-	const fetchData = async () => {
-	const formData = new FormData();
-        formData.append("question", "What are System 1 and System 2?");
-        const response = await fetch('http://127.0.0.1:8000/api/v1/ragDocumentStreamIBM', {
-            method: 'POST',
-            body: formData
-        });
-        const data = await response.json();
-        console.log(data); // Response from Backend is stored here
-	};
-	fetchData();
+	console.log("Sending question: " + question);
+	//const fetchData = async () => {
+	//const formData = new FormData();
+    //    formData.append("question", "What are System 1 and System 2?");
+    //    const response = await fetch('http://127.0.0.1:8000/api/v1/ragDocumentStreamIBM', {
+    //        method: 'POST',
+    //        body: formData
+    //    });
+    //    const data = await response.json();
+    //    console.log(data); // Response from Backend is stored here
+	//};
+	//fetchData();
   }
 
   return (
@@ -111,7 +107,15 @@ function App() {
 				<h1 id="documentHeader">Documents</h1>
 			</div>
 			<div className="Document-list">
-				<List id="documentList" list={list}></List>
+				<List id="documentList" >
+					{files.map((file) => (
+						<ListItem disablePadding key={file.uuid}>
+							<ListItemButton>
+								<ListItemText primary={file.name} />
+							</ListItemButton>
+						</ListItem>
+					))}
+				</List>
 			</div>
 			<div className="Document-buttons">
 				<Button variant="contained" id="uploadButton" onClick={handleUploadFile} startIcon={<CloudUploadIcon />}>
@@ -142,15 +146,15 @@ function App() {
 						}}
 						onKeyDown={(ev) => {
 							 if (ev.key === 'Enter') {
-								 handleSubmitQuestion(ev)
+								 handleSubmitQuestion(ev.target)
 							 }
 						}}
 						onChange={(ev) => {
-							 onQuestionChanged(ev)
+							 onQuestionChanged(ev.target.value)
 						}}
 					/>
 					<div className="Input-buttons">
-						<Button variant="contained" disabled={askDisabled} endIcon={<SendIcon />}>
+						<Button variant="contained" disabled={askDisabled} onClick={handleAskButtonClick} endIcon={<SendIcon />}>
 						  Ask
 						</Button>
 					</div>
